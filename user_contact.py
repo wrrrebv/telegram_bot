54cd478a570a93d2fc0a53650653a7a60dfa2117
wrrrebv <vlasovavasilisa20100@gmail.com>
2024-09-22 12:17:04 +0300

    создали прдеставление для контакта польщователя
    пользователь обладает именем, номерои телефона и опциональным описанием

---
+++ user_contact.py
@@ -0,0 +1,16 @@
+class UserContact:
+    
+    def __init__(self,
+                 name: str,
+                 phone_number: str,
+                 description: str ='',
+                 ):
+        self.name = name
+        self.phone_number = phone_number
+        self.description = description
+
+    def __str__(self):
+        return f'имя: {self.name}, номер телефона: {self.phone_number}'
+
+user = UserContact('Олег', '4488815882')
+print(user)
