4ded53a79a48c7e4ec705bbe90f7a1cec34c69f3
wrrrebv <156573629+wrrrebv@users.noreply.github.com>
2024-10-20 13:18:57 +0300

    добавлена рабочая версия бота для записи контактов

--- bot.py
+++ bot.py
@@ -1,23 +1,27 @@
 import datetime
 
+import telebot
 from telebot import types
 
 import config
 import user_contact
+import contact_book
 
-contacts = []
-name = None
-phone_number = None
 
+contact_builder = contact_book.ContactBuilder()
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler(commands=['start'])
-def start(message):
+def create_main_keyboard():
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
     add_contact_btn = types.KeyboardButton('добавить контакт')
     show_contact_btn = types.KeyboardButton('показать все контакты')
+    keyboard.add(add_contact_btn, show_contact_btn)
 
+    return keyboard
+
+@bot.message_handler(commands=['start'])
+def start(message):
     bot.send_message(message.chat.id, 'привет! я - бот для контактов.',
                                       reply_markup=create_main_keyboard())
     bot.register_next_step_handler(message, handle_main_commands)
@@ -26,16 +30,25 @@ def handle_main_commands(message):
     #user_message  = bot.reply_to(message, 'как зовут нового контакта?')
     #bot.register_next_step_handler(user_message, process_name_step)
 
-        if message.text == 'добавить контакт':
-            #print('пользователь захотел добавить контакт')
-            delete_keyboard = types.ReplyKeyboardRemove()
-            bot.send_messsage(message.chat.id, 'как зовут контакта?', reply_markup=delete_keyboard)
-        elif message.text == 'показать все контакты':
-            bot.send_messsage(message.chat.id, 'список всех контактов')
-            bot.register_next_step_handler(message, handle_main_commands)
+    if message.text == 'добавить контакт':
+        #print('пользователь захотел добавить контакт')
+        delete_keyboard = types.ReplyKeyboardRemove()
+        bot.send_message(message.chat.id, 'как зовут контакта?', reply_markup=delete_keyboard)
+        bot.register_next_step_handler(message, process_name_step)
+    elif message.text == 'показать все контакты':
+        contacts = contact_builder.get_contacts(message.chat.id)
+        print(f"Кол-во контактов: {len(contacts)}")
+        if len(contacts) > 0:
+            bot.send_message(message.chat.id, 'список всех контактов:')
+            for contact in contacts:
+                bot.send_message(message.chat.id, str(contact))
         else:
-            bot.send_messsage(message.chat.id, 'чо')
-            bot.register_next_step_handler(message, handle_main_commands)
+            bot.send_message(message.chat.id, 'список контактов пуст')
+
+        bot.register_next_step_handler(message, handle_main_commands)
+    else:
+        bot.send_message(message.chat.id, 'чо')
+        bot.register_next_step_handler(message, handle_main_commands)
             
 def process_name_step(message):
     name = message.text
@@ -45,7 +58,7 @@ def process_name_step(message):
         bot.register_next_step_handler(message, process_name_step)
         return
 
-    #contact_builder.add_name(name)
+    contact_builder.add_name(message.chat.id, name)
 
     bot.send_message(message.chat.id, 'номер телефона:')
     bot.register_next_step_handler(message, process_phone_number_step)
@@ -58,7 +71,7 @@ def process_phone_number_step(message):
         bot.register_next_step_handler(message, process_phone_number_step)
         return
 
-    #contact_builder.add_phone_number(name)
+    contact_builder.add_phone_number(message.chat.id, phone_number)
 
     keyboard = types.InlineKeyboardMarkup()
 
@@ -69,13 +82,16 @@ def process_phone_number_step(message):
     bot.register_next_step_handler(message, process_description_step)
 
 def process_description_step(message):
-    phone_number = message.text
+    description = message.text
 
-    if not description:
-        bot.send_message(message.chat.id,
-                         'контакт создан!',
-                         reply_markup=create_main_keyboard())
-        bot.register_next_step_handler(message, process_description_step)
+    contact_builder.add_description(message.chat.id, description)
+    contact_builder.build(message.chat.id)
+    
+    bot.send_message(message.chat.id,
+                    'контакт создан!',
+                    reply_markup=create_main_keyboard())
+    
+    bot.register_next_step_handler(message, handle_main_commands)
 
-
+    
 bot.infinity_polling()
---
+++ contact_book.py
@@ -0,0 +1,69 @@
+class UserContact:
+    
+    def __init__(self,
+                 name: str,
+                 phone_number: str = '',
+                 description: str = '',
+                 ):
+        self.name = name
+        self.phone_number = phone_number
+        self.description = description
+
+    def set_phone_number(self, phone_number: str):
+        self.phone_number = phone_number
+
+    def set_description(self, description: str):
+        self.description = description
+
+    def __str__(self):
+        return f'имя: {self.name}, номер телефона: {self.phone_number}, описание: {self.description}'
+
+
+class ContactBook:
+
+    def __init__(self) -> None:
+        self.contacts = []
+
+    def add_contact(self, contact: UserContact):
+        self.contacts.append(contact)
+
+    def find_contact(self, name: str):
+        found_contacts = []
+
+        for contact in self.contacts:
+            if contact.name == name:
+                found_contacts.append(contact)
+
+        return found_contacts
+
+    def get_contacts(self):
+        return self.contacts
+
+class ContactBuilder:
+
+    def __init__(self):
+        self.current_contact = {}
+        self.saved_contacts = {}
+
+    def get_contacts(self, chat_id):
+        if chat_id not in self.saved_contacts:
+            return []
+        return self.saved_contacts[chat_id].get_contacts()
+
+    def add_name(self, chat_id, name: str):
+        self.current_contact[chat_id] = UserContact(name)
+
+    def add_phone_number(self, chat_id, phone_number: str):
+        self.current_contact[chat_id].set_phone_number(phone_number)
+
+    def add_description(self, chat_id, description: str):
+        self.current_contact[chat_id].set_description(description)
+
+    def build(self, chat_id):
+        current_contact = self.current_contact[chat_id]
+
+        if chat_id not in self.saved_contacts:
+            self.saved_contacts[chat_id] = ContactBook()
+
+        self.saved_contacts[chat_id].add_contact(current_contact)
+        del self.current_contact[chat_id]
